9378f1a9f15abec9ba3aeb1f116d7cd95abdc11f
Jerrylum <serverofjerry@gmail.com>
2021-04-30 11:02:30 +0800

    website draft

---
+++ static/js/index.js
@@ -0,0 +1,32 @@
+new Vue({
+    el: '#Page',
+    computed: {
+        VersionName() {
+            return this.Version ? 'V' + this.Version : '';
+        }
+    },
+    created() {
+        fetch('https://api.github.com/repos/Jerrylum/Project3042/releases')
+        .then((response) => response.json())
+        .then((info) => this.HandleGitHubReleaseInfo(info));      
+    },
+    methods: {
+        HandleGitHubReleaseInfo(info) {   
+            if (info.message === 'Not Found') return;
+
+            const latestRelease = info.sort((a, b) => a.created_at > b.created_at)[0];
+            const latestAssests = latestRelease.assets;
+
+            this.Version = latestRelease.tag_name;
+
+            if (latestAssests.length != 1)
+                this.GitHubMapLink = latestRelease.html_url;
+            else
+                this.GitHubMapLink = latestAssests[0].browser_download_url;
+        }
+    },
+    data: {
+        GitHubMapLink: 'https://github.com/Jerrylum/Project3042/releases/',
+        Version: ''
+    }
+});
